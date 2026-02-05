cb64178ebc5dd1f62ce5b987fedfb23235f52729
oa22010 <opeloge.ahsam@marcellin.school.nz>
2026-02-05 14:30:56 +1300

    Created files for testing, planning and code. Placed main file in cod folder

---
+++ code/main.py
@@ -0,0 +1,11 @@
+# Import necessary modules
+import tkinter as tk
+from tkinter import ttk
+
+# Initialize the main application window
+root = tk.Tk()
+root.title("")  # Set the window title
+#root.geometry("PlaceholderxPlaceholder")    # Set the window size
+
+# Start the Tkinter main event loop to run the application
+root.mainloop()
